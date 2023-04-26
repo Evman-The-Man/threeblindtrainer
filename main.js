fda30d7305105653b94d1c231c30884637c30d27
Wouldn't you like to know <58012114+Evman-The-Man@users.noreply.github.com>
2023-04-26 09:30:05 -0400

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-let edges = new Cube();
-let corners = new OtherCube();
-
-function generateMemo(scramble) {
-    edges.doTurns(scramble);
-    corners.doTurns(scramble);
-    return [edges.generateMemo(), corners.generateMemo()];
-}
